20b5b30b5369739a87c9d49aaf6474dbe44c6adc
mikhutov <mikhutov@gmail.com>
2024-02-06 13:22:58 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676; // Цена билета
+        int mile = 20; // Стоимость одной мили
+        int bonus = price / mile; // Размер бонуса
+
+        System.out.println(bonus + " мили ");
+
+
+    }
+}
+
